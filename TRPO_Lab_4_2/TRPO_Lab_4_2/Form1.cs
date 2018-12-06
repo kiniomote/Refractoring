61e32379085f425e395d276f76543057666e9881
Kiniomote <dimon.popadin.21@yandex.ru>
2018-12-06 18:45:26 +0200

    Class

--- TRPO_Lab_4_2/TRPO_Lab_4_2/Form1.cs
+++ TRPO_Lab_4_2/TRPO_Lab_4_2/Form1.cs
@@ -17,4 +17,78 @@ namespace TRPO_Lab_4_2
 			InitializeComponent();
 		}
 	}
+
+	// Поднятие и спуск методов и полей
+
+	class Person
+	{
+		string Name { get; set; }
+		string Office { get; set; }
+		int Grants { get; set; }
+		string Adress { get; set; }
+		
+		string GiveAdressOfice()
+		{
+			return "Office: " + Office;
+		}
+
+		string GiveAdress()
+		{
+			return "Adress: " + Adress;
+		}
+	};
+
+	class Student : Person
+	{
+		int YearBorn { get; set; }
+
+		int GiveAge()
+		{
+			return 2018 - YearBorn;
+		}
+	};
+
+	class Teacher : Person
+	{
+		int YearBorn { get; set; }
+
+		int GiveAge()
+		{
+			return 2018 - YearBorn;
+		}
+	};
+
+	//_______________________________________________
+
+	class Person1
+	{
+		protected string Name { get; set; }
+		protected string Adress { get; set; }
+		protected int YearBorn { get; set; }
+
+		int GiveAge()
+		{
+			return 2018 - YearBorn;
+		}
+
+		string GiveAdress()
+		{
+			return "Adress: " + Adress;
+		}
+	};
+
+	class Student1 : Person1
+	{
+		int Grants { get; set; }
+	};
+
+	class Teacher1 : Person1
+	{
+		string Office { get; set; }
+
+		string GiveAdressOfice()
+		{
+			return "Office: " + Office;
+		}
+	};
 }
